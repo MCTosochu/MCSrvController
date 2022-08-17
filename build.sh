94a7aef70447fa8d937cd98e39da94e27a13b5ed
AsPulse <84216737+AsPulse@users.noreply.github.com>
2022-08-17 16:14:08 +0900

    Feature/arm build (#2)
    * FIx: build script
    
    * Fix: acions

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 mkdir ./build -p
-GOOS=linux GOARCH=amd64 go build -o build/mcsrv_linux main.go
+GOOS=linux GOARCH=amd64 go build -o build/mcsrv_linux_amd64 main.go
+GOOS=linux GOARCH=arm64 go build -o build/mcsrv_linux_arm64 main.go
 GOOS=windows GOARCH=amd64 go build -o build/mcsrv_win.exe main.go
